fc01d6e48f7c94d9cf401cadbedb919069300a9b
monira15 <monira.programmer@gmail.com>
2023-09-02 00:06:53 +0600

    seccond commit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 const handleCatagory =async()=>{
    
    const response = await fetch('https://openapi.programming-hero.com/api/videos/categories');
@@ -14,8 +16,10 @@ const handleCatagory =async()=>{
    
 };
 
+
  const handlelodeyoutube=async(catagoryId)=>{
   console.log(catagoryId);
+  
      const response=await fetch(`  https:openapi.programming-hero.com/api/videos/category/${catagoryId}`);
     const data = await response.json();
     //  console.log(data.data);
@@ -27,19 +31,22 @@ const handleCatagory =async()=>{
       //  console.log(news);
        const div1 =document.createElement('div');
        div1.innerHTML =`
-       <div class="card lg:w-[320px] lg:h-[350px]  bg-base-100 shadow-xl ">
-       <figure><img class="pb-20 lg:w-[470px] lg:h-[320px]" src=${news.thumbnail} /></figure>
+       <div class="card lg:w-[320px] h-[350px]  bg-base-100  rounded-none">
+       <figure><img class="lg:-pb-24 md:w-[300px] w-[470px] lg:h-[200px] " src=${news.thumbnail} /></figure>
        <div class="card-body">
-       <div class="flex space-between items-center gap-4 ">
+       <div class="flex justify-start lg:-ml-6  items-center gap-4 ">
             <img class="w-12 h-12 rounded-full" src=${news.authors[0].profile_picture} />
             <h3>${news.title}</h3>
           
           </div>
-     <div>
-     <h3>${news?.authors[1]?.profile_name}</h3>
-     <h3>${news.others.views}</h3>
+     <div class="ml-8 flex gap-2 items-center">
+     <h3>${news.authors[0].profile_name}</h3>
+     <p>${news.authors[0].verified ? '<span class="badge-icon bg-red-500 rounded-xl"><img class="w-[20px]" src="./budge-icon2.png" alt=""></span>' : ''}</p>
+
+
+   
      </div>
-          
+     <h3 class="ml-8" >${news.others.views} veiws</h3>
        </div>
      </div>
        `;
@@ -48,9 +55,19 @@ const handleCatagory =async()=>{
     
     
     }
- 
- 
+
+
+    function toggleContent() {
+      var content = document.getElementById("content");
+      if (content.style.display === "none" || content.style.display === "") {
+          content.style.display = "block";
+      } else {
+          content.style.display = "none";
+      }
+  }
 
 
 handleCatagory();
+
 handlelodeyoutube(1000)
+
